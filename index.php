69ab855dac517b9db8fcf81222c067f985a309e8
BOUKOMBA-Jordan <jordantalla43@gmail.com>
2024-02-27 11:24:07 +0100

    mettre sur le git

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
         <link rel="stylesheet" href="./styles/logo.css" />
         <link rel="stylesheet" href="./styles/index.css" />
         <link rel="stylesheet" href="./styles/contact.css" />
+        <link rel="stylesheet" href="./styles/A_propos_de_moi.css" />
+       
     </head>
 
     <body>
@@ -25,6 +27,51 @@
             </div>
             <p class="sous-titre">Des développeurs habilent et chevronnés </p>
         </section>
+
+
+
+
+
+        <div class="frame-17-7yQ">
+        <div class="auto-group-m3pa-eCe">
+            <div class="auto-group-9n14-NPY">
+                <div class="paragraphe-gf8">Je me présente : </div>
+                <div class="titre-de-section-AaJ">Jordan TALLA</div>
+            </div>
+            <div class="auto-group-dh82-TZQ">
+                <div class="paragraphe-158">
+                </div>
+                <p class="jeune-designer-ui-ux-passionn-du-design-mobile-du-dveloppement-web-et-application-mobile-mais-aussi-pdg-de-la-jordanbrel-corporation-Kbc">
+                    jeune designer UI/UX
+                    <br />
+
+                    passionné du design mobile, du
+                    <br />
+
+                    développement web et application mobile.
+                    <br />
+
+                    Mais aussi PDG de la Jordanbrel
+                    <br />
+
+                    corporation.
+                </p>
+            </div>
+        </div>
+        <div class="c17cc67f5e6810524f38e963b2af821b-1-A6S">
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
 
         <footer class="contact">
             <?php include("contact.php"); ?>
--- menu.php
+++ menu.php
@@ -2,10 +2,10 @@
 <nav class="menu-header">
 <div class="menu">
   <a href="index.php" class="menu-bouton">Accueil</a>
-  <a href="A_propos_de_moi.php" class="menu-bouton">À propos</a>
+ 
   <a href="Réalisation.php" class="menu-bouton">Réalisation</a>
   <a href="#" class="menu-bouton">Compétence</a>
-  <a href="#" class="menu-bouton">Formation /certificat</a>
+  <a href="#" class="menu-bouton">Formation</a>
   <a href="#" class="menu-bouton">Contact</a>
 </div>
 </nav>
